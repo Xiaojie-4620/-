230afa539fdfe131ad0e22ec2eab69184154b350
小杰 <3140414515@qq.com>
2022-11-28 20:54:45 +0800

    "upload"

---
+++ springmvc_11_page/src/main/java/com/xiaoyu/config/MyBatisConfig.java
@@ -0,0 +1,26 @@
+package com.xiaoyu.config;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource){
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setTypeAliasesPackage("com.xiaoyu.domain");
+        return factoryBean;
+    }
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer(){
+        MapperScannerConfigurer msc = new MapperScannerConfigurer();
+        msc.setBasePackage("com.xiaoyu.dao");
+        return msc;
+    }
+
+}
